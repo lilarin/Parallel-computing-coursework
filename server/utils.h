7ff5d3e85d91207f39b8658267f7580059e95f5e
lilarin <80650911+lilarin@users.noreply.github.com>
2024-12-23 01:28:55 +0200

    Added enum and structures for a web server

--- server/utils.h
+++ server/utils.h
@@ -9,4 +9,20 @@ struct Document {
     std::string content;
 };
 
+enum class RequestType : std::uint8_t {
+    UPLOAD_FILES,
+    SEARCH,
+    DELETE_FILE
+};
+
+struct Request {
+    RequestType type;
+    std::string data;
+};
+
+struct Response {
+    int status_code;
+    std::string data;
+};
+
 #endif // TYPES_H

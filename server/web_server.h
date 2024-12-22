9fe4caf80855e76660d846f898536e7e6511568a
lilarin <80650911+lilarin@users.noreply.github.com>
2024-12-23 01:30:49 +0200

    Implement structure of the web server class

---
+++ server/web_server.h
@@ -0,0 +1,33 @@
+#ifndef WEB_SERVER_H
+#define WEB_SERVER_H
+
+#include <string>
+#include "indexer.h"
+#include "thread_pool.h"
+
+class InvertedIndex;
+class FileStorage;
+class Indexer;
+class ThreadPool;
+struct Request;
+struct Response;
+
+class WebServer {
+private:
+    int port;
+    InvertedIndex& inverted_index;
+    FileStorage& file_storage;
+    Indexer indexer;
+    ThreadPool thread_pool;
+
+    Response handleFileUpload(const Request& request);
+    Response handleDeleteFileRequest(const Request& request);
+    Response handleSearchRequest(const Request& request);
+    void handleClient(int client_socket);
+
+public:
+    WebServer(int port, InvertedIndex& index, FileStorage& storage, int num_threads = 4);
+    [[noreturn]] void start();
+};
+
+#endif // WEB_SERVER_H

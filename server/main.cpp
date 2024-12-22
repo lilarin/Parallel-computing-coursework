9e76012b89cb1b0c808acedb81717ee502e2c259
lilarin <80650911+lilarin@users.noreply.github.com>
2024-12-23 01:31:59 +0200

    Update main entrypoint to start up the web server

--- server/main.cpp
+++ server/main.cpp
@@ -9,4 +9,7 @@ int main() {
     Indexer indexer(inverted_index, file_storage);
     std::vector<std::string> initial_files = file_storage.getAllFiles();
     indexer.buildIndex(initial_files);
+
+    WebServer web_server(8080, inverted_index, file_storage, 6);
+    web_server.start();
 }

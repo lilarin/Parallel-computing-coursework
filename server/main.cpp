036d0c60438021f005306043fc1822b2486c608b
lilarin <80650911+lilarin@users.noreply.github.com>
2024-12-29 15:06:52 +0200

    Changed default amount of the threads to use in web server

--- server/main.cpp
+++ server/main.cpp
@@ -10,6 +10,6 @@ int main() {
     std::vector<std::string> initial_files = file_storage.getAllFiles();
     indexer.buildIndex(initial_files);
 
-    WebServer web_server(8080, inverted_index, file_storage, 6);
+    WebServer web_server(8080, inverted_index, file_storage, 12);
     web_server.start();
 }

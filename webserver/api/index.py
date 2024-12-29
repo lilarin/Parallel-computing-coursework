4e6bd784a1ada63837223ebcbd94d5ddc6bfbe88
lilarin <80650911+lilarin@users.noreply.github.com>
2024-12-29 18:32:44 +0200

    Refactored code to properly handle responses

--- webserver/api/index.py
+++ webserver/api/index.py
@@ -1,41 +1,36 @@
 from fastapi import APIRouter, Query
 
 from client.client import client
-from webserver.schemas import ServerResponse, UploadFile
+from webserver.middleware import parse_response_middleware
+from webserver.schemas import ServerResponse, UploadFile, ServerRawResponse
 
 router = APIRouter()
 
 
 @router.post("/upload")
+@parse_response_middleware
 async def upload_file(request: UploadFile) -> ServerResponse:
     filename = request.filename
     content = request.content
 
     status, data = await client.upload_file(filename, content)
-    return ServerResponse(
-        status_code=status,
-        response=data,
-    )
+    return ServerRawResponse(response=data, status_code=status)
 
 
 @router.get("/search")
+@parse_response_middleware
 async def search_files(
         content: str = Query(..., description="The content to search")
 ) -> ServerResponse:
     status, data = await client.search_files(content)
-    return ServerResponse(
-        status_code=status,
-        response=data,
-    )
+    return ServerRawResponse(response=data, status_code=status)
 
 
 @router.delete("/delete")
+@parse_response_middleware
 async def delete_file(
         file_name: str = Query(..., description="The filename to delete from index")
 ) -> ServerResponse:
     status, data = await client.delete_file(file_name)
-    return ServerResponse(
-        status_code=status,
-        response=data,
-    )
+    return ServerRawResponse(response=data, status_code=status)
 

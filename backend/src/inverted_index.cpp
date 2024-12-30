59d5155e2ae68b923f64cbd32816667ab670aeb7
lilarin <80650911+lilarin@users.noreply.github.com>
2024-12-30 10:14:22 +0200

    refactor: implemented some sort of the ConcurrentHashMap instead of the regular hashmap and one mutex

--- backend/src/inverted_index.cpp
+++ backend/src/inverted_index.cpp
@@ -1,39 +1,40 @@
-#include "inverted_index.h"
 #include <iostream>
 #include <sstream>
 #include <algorithm>
-#include <shared_mutex>
 #include <mutex>
+#include "inverted_index.h"
 
 void InvertedIndex::addDocument(const Document& document) {
-    std::unique_lock<std::shared_mutex> lock(index_mutex);
     std::cout << "DEBUG: Adding document '" << document.filename << "' to index." << std::endl;
     std::stringstream ss(document.content);
     std::string word;
     while (ss >> word) {
-        index[word].insert(document.filename);
+        size_t bucket_index = getBucketIndex(word);
+        std::lock_guard<std::mutex> lock(bucket_mutexes[bucket_index]);
+        buckets[bucket_index][word].insert(document.filename);
     }
     std::cout << "DEBUG: Document '" << document.filename << "' added to index." << std::endl;
 }
 
 void InvertedIndex::removeDocument(const std::string& filename) {
-    std::unique_lock<std::shared_mutex> lock(index_mutex);
     std::cout << "DEBUG: Removing document '" << filename << "' from index." << std::endl;
-    for (auto& pair : index) {
-        pair.second.erase(filename);
-    }
-    for (auto it = index.begin(); it != index.end(); ) {
-        if (it->second.empty()) {
-            it = index.erase(it);
-        } else {
-            ++it;
+    for (int i = 0; i < NUM_BUCKETS; ++i) {
+        std::lock_guard<std::mutex> lock(bucket_mutexes[i]);
+        for (auto& pair : buckets[i]) {
+            pair.second.erase(filename);
+        }
+        for (auto it = buckets[i].begin(); it != buckets[i].end(); ) {
+            if (it->second.empty()) {
+                it = buckets[i].erase(it);
+            } else {
+                ++it;
+            }
         }
     }
     std::cout << "DEBUG: Document '" << filename << "' removed from index." << std::endl;
 }
 
 std::set<std::string> InvertedIndex::search(const std::string& query) {
-    std::shared_lock<std::shared_mutex> lock(index_mutex);
     std::cout << "INFO: Searching index for query: '" << query << "'" << std::endl;
     std::stringstream ss(query);
     std::string word;
@@ -41,8 +42,10 @@ std::set<std::string> InvertedIndex::search(const std::string& query) {
     bool first_word = true;
 
     while (ss >> word) {
-        auto it = index.find(word);
-        if (it != index.end()) {
+        size_t bucket_index = getBucketIndex(word);
+        std::lock_guard<std::mutex> lock(bucket_mutexes[bucket_index]);
+        auto it = buckets[bucket_index].find(word);
+        if (it != buckets[bucket_index].end()) {
             if (first_word) {
                 results = it->second;
                 first_word = false;
@@ -64,22 +67,26 @@ std::set<std::string> InvertedIndex::search(const std::string& query) {
 }
 
 void InvertedIndex::clear() {
-    std::unique_lock<std::shared_mutex> lock(index_mutex);
     std::cout << "INFO: Clearing the inverted index." << std::endl;
-    index.clear();
+    for (int i = 0; i < NUM_BUCKETS; ++i) {
+        std::lock_guard<std::mutex> lock(bucket_mutexes[i]);
+        buckets[i].clear();
+    }
     std::cout << "INFO: Inverted index cleared." << std::endl;
 }
 
 void InvertedIndex::printIndexes() {
-    std::shared_lock<std::shared_mutex> lock(index_mutex);
-    if (!index.empty()) {
+    if (!buckets.empty()) {
         std::cout << "--- Inverted Index ---" << std::endl;
-        for (const auto& pair : index) {
-            std::cout << pair.first << ": ";
-            for (const auto& filename : pair.second) {
-                std::cout << filename << " ";
+        for (int i = 0; i < NUM_BUCKETS; ++i) {
+            std::lock_guard<std::mutex> lock(bucket_mutexes[i]);
+            for (const auto& pair : buckets[i]) {
+                std::cout << pair.first << ": ";
+                for (const auto& filename : pair.second) {
+                    std::cout << filename << " ";
+                }
+                std::cout << std::endl;
             }
-            std::cout << std::endl;
         }
         std::cout << "----------------------" << std::endl;
     }
--- backend/src/inverted_index.h
+++ backend/src/inverted_index.h
@@ -4,15 +4,26 @@
 #include <unordered_map>
 #include <set>
 #include <string>
-#include <shared_mutex>
+#include <mutex>
+#include <vector>
+#include <functional>
 #include "utils.h"
 
 class InvertedIndex {
 private:
-    std::unordered_map<std::string, std::set<std::string>> index;
-    std::shared_mutex index_mutex;
+
+    static const int NUM_BUCKETS = 16;
+    std::vector<std::unordered_map<std::string, std::set<std::string>>> buckets;
+    std::vector<std::mutex> bucket_mutexes;
+
+    static size_t getBucketIndex(const std::string& word) {
+        std::hash<std::string> hasher;
+        return hasher(word) % NUM_BUCKETS;
+    }
 
 public:
+    InvertedIndex() : buckets(NUM_BUCKETS), bucket_mutexes(NUM_BUCKETS) {}
+
     void addDocument(const Document& document);
     void removeDocument(const std::string& filename);
     std::set<std::string> search(const std::string& query);
